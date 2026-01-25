3876079a7f3202a269199233f6b388fb3175a36f
Naveengupta2006 <naveengupta931844@gmail.com>
2026-01-25 14:15:50 +0530

    Data Understanding

--- notebooks/Analysis.sql
+++ notebooks/Analysis.sql
@@ -1,1 +1,20 @@
 -- Active: 1769327443937@@127.0.0.1@3306@supply_chain
+select *from supply_chain
+
+-- Data understanding
+-- check total records
+-- check unique skus
+-- check missing values in key columns
+
+select count(*) as total_records
+from supply_chain
+
+select DISTINCT SKU 
+from supply_chain
+
+
+select 
+    sum(CASE WHEN 'Product type' is null then 1 else 0 end) as missing_product,
+    sum(case when 'location' is null then 1 else 0 end) as missing_location,
+    sum(case when 'Number of products sold' is null then 1 else 0 end) as missing_sold_product
+from supply_chain     
